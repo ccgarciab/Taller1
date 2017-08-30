09cfa3af0a54ea33b4e126c51187176a51932592
ESTUDIANTE <ESTUDIANTE@DESKTOP-KTO51U0>
2017-08-30 15:33:34 -0500

    Agregado metodos para encontrar los mejores y peores libros

--- src/tallerlibros/ConjuntoLibros.java
+++ src/tallerlibros/ConjuntoLibros.java
@@ -34,16 +34,54 @@ public class ConjuntoLibros {
     }
     
     //correrLibros elimina los espacios vacios entre los libros, dejandolos todos al final del arreglo
-    public void correrLibros(){
-        for(int i=0; i<this.numeroLibros; ++i){
-            if(this.libros[i].equals(null)){
-                for(int j=i; j<this.numeroLibros; ++j){
-                    if(this.libros[j]!=null)
-                        this.libros[i]=this.libros[j];
-                }
+   
+    
+    public Libro[] getPeoresLibros(){
+        Libro tempLibro=null;
+        int calificacion=10;
+        int numeroDePeores=0;
+        Libro peoresLibros[]=new Libro[numeroDePeores];
+        for(int i=0; i<numeroLibros; ++i){
+            if(this.libros[i].getCalificacion()<calificacion){
+             calificacion=this.libros[i].getCalificacion();
             }
         }
+        for(int i=0; i<numeroLibros; ++i){
+            if(this.libros[i].getCalificacion()==calificacion){
+             numeroDePeores=numeroDePeores+1;
+            }
+        }
+        for(int i=0; i<numeroLibros; ++i){
+            if(this.libros[i].getCalificacion()==calificacion){
+             peoresLibros[i]=this.libros[i];
+            }
+        }
+        return peoresLibros;
     }
+    
+    public Libro[] getMejoresLibros(){
+        Libro tempLibro=null;
+        int calificacion=0;
+        int numeroDeMejores=0;
+        Libro mejoresLibros[]=new Libro[numeroDeMejores];
+        for(int i=0; i<numeroLibros; ++i){
+            if(this.libros[i].getCalificacion()>calificacion){
+             calificacion=this.libros[i].getCalificacion();
+            }
+        }
+        for(int i=0; i<numeroLibros; ++i){
+            if(this.libros[i].getCalificacion()==calificacion){
+             numeroDeMejores=numeroDeMejores+1;
+            }
+        }
+        for(int i=0; i<numeroLibros; ++i){
+            if(this.libros[i].getCalificacion()==calificacion){
+             mejoresLibros[i]=this.libros[i];
+            }
+        }
+        return mejoresLibros;
+    }
+    
     
     private int numeroLibros;
     private int librosGuardados;
